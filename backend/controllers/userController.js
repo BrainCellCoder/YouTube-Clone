5578f3b456a498f75833f75143c6ebcfcb83366e
BrainCellCoder <00abhisheksharma1999@gmail.com>
2023-06-11 19:53:21 +0530

    feat: updateUser controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,35 @@
-exports.test = (req, res) => {
+const { createError } = require("../error");
+const User = require("../models/userModel");
+
+exports.updateUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+    return res.status(200).json(updatedUser);
+  } else {
+    return next(createError(403, "You can update only your account!"));
+  }
+};
+
+exports.deleteUser = (req, res, next) => {
+  res.send("USer ROute");
+};
+exports.getUser = (req, res, next) => {
+  res.send("USer ROute");
+};
+exports.subscribe = (req, res, next) => {
+  res.send("USer ROute");
+};
+
+exports.unsubscribe = (req, res, next) => {
+  res.send("USer ROute");
+};
+exports.like = (req, res, next) => {
+  res.send("USer ROute");
+};
+exports.dislike = (req, res, next) => {
   res.send("USer ROute");
 };
--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,8 +1,15 @@
 const userController = require("../controllers/userController");
 
 const express = require("express");
+const { verifyToken } = require("../verifyToken");
 const router = express.Router();
 
-router.route("/test").get(userController.test);
+router.route("/:id").put(verifyToken, userController.updateUser);
+router.route("/:id").delete(userController.deleteUser);
+router.route("/find/:id").get(userController.getUser);
+router.route("/sub/:id").put(userController.subscribe);
+router.route("/unsub/:id").put(userController.unsubscribe);
+router.route("/like/:videoId").put(userController.like);
+router.route("/dislike/:videoId").put(userController.dislike);
 
 module.exports = router;
