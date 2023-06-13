0957efb879546ae08ca9fff38f12d19a43f0f16f
BrainCellCoder <00abhisheksharma1999@gmail.com>
2023-06-13 11:57:55 +0530

    feat: addComment,deleteComment,getComment controll

--- backend/controllers/commentControllers.js
+++ backend/controllers/commentControllers.js
@@ -1,3 +1,50 @@
-exports.test = (req, res) => {
-  res.send("Comment ROute");
+const { createError } = require("../error");
+const Comment = require("../models/commentModel");
+const Video = require("../models/videoModel");
+
+exports.addComment = async (req, res, next) => {
+  try {
+    const { videoId, desc } = req.body;
+    const comment = await Comment.create({
+      userId: req.user.id,
+      videoId,
+      desc,
+    });
+    res.status(200).json({
+      success: true,
+      comment,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.deleteComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.id);
+    const video = await Video.findById(req.params.id);
+    if (req.user.id === comment.userId || req.user.id === video.userId) {
+      await Comment.findByIdAndDelete(req.params.id);
+      return res.status(200).json({
+        success: true,
+        message: "Comment has been deleted!",
+      });
+    } else {
+      return next(createError(403, "You can delete only your comment!"));
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getComments = async (req, res, next) => {
+  try {
+    const comments = await Comment.find({ videoId: req.params.videoId });
+    res.status(200).json({
+      success: true,
+      comments,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
--- backend/routes/commentsRoutes.js
+++ backend/routes/commentsRoutes.js
@@ -1,8 +1,11 @@
 const commentController = require("../controllers/commentControllers");
 
 const express = require("express");
+const { verifyToken } = require("../verifyToken");
 const router = express.Router();
 
-router.route("/test").get(commentController.test);
+router.route("/").post(verifyToken, commentController.addComment);
+router.route("/:id").delete(verifyToken, commentController.deleteComment);
+router.route("/:videoId").get(commentController.getComments);
 
 module.exports = router;
